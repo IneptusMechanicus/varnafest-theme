2cb29abe07e48499442db6598543081eb45d386b
Ivo Gerginov <ivo.gerginov9806@gmail.com>
2020-09-07 17:34:12 +0300

    Removing WP bar from non-admins

--- functions.php
+++ functions.php
@@ -189,3 +189,10 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
